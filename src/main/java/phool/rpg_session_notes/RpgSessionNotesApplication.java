def081e87ec4fba02a2724e6668cc96941003bba
Phoolis <carlsonps@gmail.com>
2024-09-26 17:57:34 +0300

    Initial commit

---
+++ src/main/java/phool/rpg_session_notes/RpgSessionNotesApplication.java
@@ -0,0 +1,13 @@
+package phool.rpg_session_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgSessionNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgSessionNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/phool/rpg_session_notes/RpgSessionNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package phool.rpg_session_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgSessionNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
