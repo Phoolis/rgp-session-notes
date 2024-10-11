871ccfae94e3b82ede1324a0565302b7726eec18
Phoolis <carlsonps@gmail.com>
2024-10-11 17:09:23 +0300

    Add header th-fragment, index page and pagecontroller UNFINISHED

---
+++ src/main/java/phool/rpg_session_notes/web/PageController.java
@@ -0,0 +1,14 @@
+package phool.rpg_session_notes.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+}
